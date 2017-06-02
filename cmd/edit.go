e6cd013c0aaca94bcb0fcdadef5361e8db2ad246
b4b4r07 <b4b4r07@gmail.com>
2017-06-02 20:45:22 +0900

    Add edit command

---
+++ cmd/edit.go
@@ -0,0 +1,21 @@
+package cmd
+
+import (
+	"github.com/b4b4r07/history/cli"
+	"github.com/spf13/cobra"
+)
+
+var editCmd = &cobra.Command{
+	Use:   "edit",
+	Short: "Edit",
+	Long:  "Edit",
+	RunE:  edit,
+}
+
+func edit(cmd *cobra.Command, args []string) error {
+	return cli.Edit(cli.Conf.History.Path)
+}
+
+func init() {
+	RootCmd.AddCommand(editCmd)
+}
